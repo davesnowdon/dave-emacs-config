52059dff159c69822038b57a889f221f5b9e33fe
Dave Snowdon <dave@davesnowdon.com>
2018-07-01 18:10:58 +0100

    Initial config based on https://dev.to/huytd/emacs-from-scratch-1cg6

---
+++ init.el
@@ -0,0 +1,34 @@
+;; Minimal UI
+;(scroll-bar-mode -1)  - I like have scroll bars
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+;; Emacs by default will pester you with a splash screen and a message in the echo area. To disable both, and have emacs default to its *scratch* buffer,
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t) 
+
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :init
+  (load-theme 'doom-one t)
+  :config
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
